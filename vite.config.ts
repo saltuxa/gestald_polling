99fb3a883cc74c3a65406593f86ad92dffab97d1
saltuxa <popcaric@gmail.com>
2026-05-10 01:03:29 +0200

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   build: {
     outDir: "dist/client",
